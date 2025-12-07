c0cbbba351dfc9aa1ec025b2718cbe526a8ee42d
Chaugiakiet <kiecgse180513@fpt.edu.vn>
2025-12-07 21:12:41 +0700

    fix bug merge for FE

--- travel-guide-frontend/src/pages/LandingPage/LandingPage.jsx
+++ travel-guide-frontend/src/pages/LandingPage/LandingPage.jsx
@@ -14,6 +14,7 @@ import {
   ChevronDown,
   CheckCircle2
 } from 'lucide-react';
+import { useLanguage } from '../../context/LanguageContext';
 import './LandingPage.css';
 import { useScrollAnimation } from './useScrollAnimation';
 
